c0dfdcd1d5e94eb1c8f43defabc211b544c78f99
Alok Jha <aalok2147@gmail.com>
2025-09-01 16:13:23 +0530

    Create create_tables.sql

---
+++ basics/create_tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE employees (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary DECIMAL(10,2)
+);
+
+CREATE TABLE departments (
+    id INT PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
